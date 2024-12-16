a7f5aac6a59df0c302ff9395070d682834b17b70
Julia <alu-julgalfer@foc.es>
2024-12-16 17:42:54 +0100

    Se modifican nivel1/listas_4 y listas_6

--- Nivel_1/listas_4.py
+++ Nivel_1/listas_4.py
@@ -1,5 +1,5 @@
 abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-abecedario_rest = [letra for i, letra in enumerate(abecedario) if (i + 1) % 3 != 0]
+abecedario_rest = [(letra,str(i +1 )) for i, letra in enumerate(abecedario) if (i + 1) % 3 != 0]
 
 
 print ("Lista tras eliminar las letras en posiciones de múltiplos de 3: ")
